40badb2935b93e1e5676e24535cb0ee68a76ac80
Steven Garberg <steven.garberg.jr@gmail.com>
2022-01-06 23:15:41 -0500

    Added post stream project

---
+++ Assets/Scripts/SoundEffectButton.cs
@@ -0,0 +1,61 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+[RequireComponent(typeof(AudioSource))]
+public class SoundEffectButton : MonoBehaviour
+{
+    private GameObject _gameObject;
+    private Button _button;
+    private TextMeshProUGUI _text;
+    private AudioSource _audioSource;
+
+    public string Name => _text.text;
+    public bool IsPlaying => _audioSource.isPlaying;
+
+    private void Awake()
+    {
+        _gameObject = gameObject;
+        _button = GetComponent<Button>();
+        _text = GetComponentInChildren<TextMeshProUGUI>();
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Start()
+    {
+        _button.onClick.AddListener(() =>
+        {
+            Debug.Log("Sound effect button was clicked");
+            Play();
+        });
+    }
+
+    public SoundEffectButton Construct(AudioClip clip)
+    {
+        _gameObject.name = $"{_gameObject.name} - {clip.name}";
+        _audioSource.clip = clip;
+        _text.SetText(clip.name);
+        return this;
+    }
+
+    public void Play()
+    {
+        if (_audioSource.clip != null)
+        {
+            Debug.Log($"Playing {_audioSource.clip.name}");
+            SoundboardManager.Instance.Play?.Invoke();
+            _audioSource.Play();
+        }
+    }
+    
+    public void Stop()
+    {
+        _audioSource.Stop();
+    }
+
+    public void SetLoop(bool shouldLoop)
+    {
+        _audioSource.loop = shouldLoop;
+    }
+}
---
+++ Assets/Scripts/SoundboardManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class SoundboardManager : MonoBehaviour
+{
+    public static SoundboardManager Instance { get; private set; }
+    public UnityAction Play { get; private set; }
+    
+    [SerializeField] private GameObject _soundEffectButtonPrefab;
+    [SerializeField] private Transform _soundEffectButtonParentTransform;
+    [SerializeField] private Toggle _stopPreviousToggle, _loopToggle;
+    [SerializeField] private Button _randomButton;
+    private ushort _buttonCount;
+    
+    private readonly List<SoundEffectButton> _soundEffectButtons = new List<SoundEffectButton>();
+    
+    private bool ShouldStopPrevious => _stopPreviousToggle.isOn;
+    
+    private void Awake()
+    {
+        Instance = this;
+    }
+    
+    private void Start()
+    {
+        Play += OnPlay;
+        
+        var soundEffects = Resources.LoadAll<AudioClip>("Sounds");
+        if (soundEffects == null || !soundEffects.Any())
+        {
+            Debug.Log("No sound effects could be found.");
+        }
+        else
+        {
+            Debug.Log($"{soundEffects.Length} sound effects were found.");
+            foreach (var soundEffect in soundEffects)
+            {
+                _soundEffectButtons.Add(Instantiate(_soundEffectButtonPrefab, _soundEffectButtonParentTransform)
+                    .GetComponent<SoundEffectButton>()
+                    .Construct(soundEffect));
+                
+                Debug.Log($"Sound effect button was created for '{soundEffect.name}'");
+                _buttonCount++;
+            }
+        }
+        
+        _loopToggle.onValueChanged.AddListener(isOn =>
+        {
+            Debug.Log($"Loop toggled to {isOn}");
+            foreach (var soundEffectButton in _soundEffectButtons)
+            {
+                Debug.Log($"Toggling loop for {soundEffectButton.Name} to {isOn}");
+                soundEffectButton.SetLoop(isOn);
+            }
+        });
+        
+        _randomButton.onClick.AddListener(() =>
+        {
+            Debug.Log("Playing random");
+            _soundEffectButtons[Random.Range(0, _buttonCount)].Play();
+        });
+    }
+
+    private void OnPlay()
+    {
+        Debug.Log("OnPlay callback");
+        if (ShouldStopPrevious)
+        {
+            Debug.Log("Stopping previous");
+            foreach (var soundEffectButton in _soundEffectButtons.Where(b => b.IsPlaying))
+            {
+                Debug.Log($"Stopping '{soundEffectButton.Name}'");
+                soundEffectButton.Stop();
+            }
+        }
+    }
+}
